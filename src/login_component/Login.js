e562869af5f3ab7b1f02745e8d6784ffda8640b3
Programmer2 <johangunawan817@gmail.com>
2022-01-11 19:38:53 +0700

    Add Login and Register Form

--- src/login_component/Login.js
+++ src/login_component/Login.js
@@ -1,10 +1,51 @@
-function Login() {
-    return (
-      <div className="login-container">
-        
-      </div>
-    );
-  }
-  
-  export default Login;
-  
+import React from "react";
+
+class Login extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            valueUsername : '',
+            valuePassword : '' 
+        }
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            valueUsername : event.target.value
+        })
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({
+            valuePassword : event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert(`Username : ${this.state.valueUsername} Password : ${this.state.valuePassword}`);
+    }
+    
+
+    render(){
+        return (
+            <div className="login-container container col-md-5 col-12 mt-5 bg-light rounded shadow p-4">
+                <h1>Login Form</h1>
+                <hr />
+                <form onSubmit={this.handleSubmit}>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" className="form-control" id="username" value={this.state.valueUsername} onChange={this.handleUsernameChange} />
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" value={this.state.valuePassword} onChange={this.handlePasswordChange} />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/register_component/Register.js
+++ src/register_component/Register.js
@@ -1,10 +1,69 @@
-function Register() {
-    return (
-      <div className="register_container">
-        <h1>Register Page</h1>
-      </div>
-    );
-  }
-  
+import React from "react";
+
+class Register extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+            valueUsername : '',
+            valuePassword : '',
+			valueChangePassword : ''
+        }
+	}
+
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            valueUsername : event.target.value
+        })
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({
+            valuePassword : event.target.value
+        })
+    }
+
+	handleConfirmPasswordChange = (event) => {
+		this.setState({
+			valueChangePassword : event.target.value
+		})
+	}
+
+    handleSubmit = (event) => {
+        alert(`Username : ${this.state.valueUsername} Password : ${this.state.valuePassword}`);
+    }
+
+	render(){
+		return (
+			<div className="register_container container col-5 mt-5 bg-light rounded shadow p-4">
+				<h1>Registration Form</h1>
+				<hr />
+				<form onSubmit={this.handleSubmit}>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" className="form-control" id="username" value={this.state.valueUsername} onChange={this.handleUsernameChange} />
+						<div className="invalid-feedback">
+							{this.usernameError}
+						</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" value={this.state.valuePassword} onChange={this.handlePasswordChange} />
+						<div className="invalid-feedback">
+							{this.passwordError}
+						</div>
+                    </div>
+					<div class="mb-3">
+                        <label for="password" class="form-label">Confirm-Password</label>
+                        <input type="password" class="form-control" id="password" value={this.state.valuePassword} onChange={this.handlePasswordChange} />
+						<div className="invalid-feedback">
+							{this.confirmPasswordError}
+						</div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Register</button>
+                </form>
+			</div>
+		)
+	}
+}
 export default Register;
-  
