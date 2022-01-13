97e67714079ff6c48301bb7e8792f1e7843d6ae9
unknown <johangunawan817@gmail.com>
2022-01-13 23:47:04 +0700

    Add MOVIE API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
-
-import Navbar from './navbar_component/Navbar';
-import Login from './login_component/Login';
-import Register from './register_component/Register';
-
+import Navbar from './components/navbar_component/Navbar';
+import Login from './components/login_component/Login';
+import Register from './components/register_component/Register';
+import ListCard from './components/card_component/ListCard';
 
 function App() {
 	return (
@@ -14,6 +14,7 @@ function App() {
 					<Route path="/login" element={<Login />} />
 					<Route path="/register" element={<Register />} />
 				</Routes>
+				<ListCard />
 			</div>
 		</BrowserRouter>
 	);
